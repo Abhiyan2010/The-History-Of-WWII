4582714187419dfa58f1db8974191452324176a6
Abhiyan2010 <138946842+Abhiyan2010@users.noreply.github.com>
2023-07-21 21:08:16 -0700

    Add files via upload

---
+++ WWII.py
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title> World War II Explained </title>
+</head
+<body>
+<h1>World War 2 is a global conflict in between 1940 - 1944. In that Time there were 2 sides: The Allies & The Axis</hl>
+
+<h2> The Cause Of WW2: </h2>
+<img scr ="World War 2" height ="1200" width "810">
+
+<p>WW2 started after the Nazis, led by Adolf Hitler, attacked Poland, an unprotected nation. This caused a chain reaction for other great Nations like Great Britain, France, The Soviet Union led by Russia, The United States & China. They fought together as the Allies. They fought against the Axis, which were nations Germany, Japan & Italy.
+
+<h2> The Affects of World War II:<h/h2>
+<ol>
+        <li> WW2 led to the deaths of 56 million military soliders & 38 million civillians
+        <li> Citizens of Countries that weren't involved were slowly executed by the Axis
+        <li> Even with their intention to rule the land, the Axis ended up losing the War
+</ol>
+
+<h3> How the Allies Defeated the Axis:
+<ol>
+        <li> The Allies were very close to losing the war to the Axis, but won by using their Air & Sea power. 
+        <li> Due to the Allies' Air & Sea power, they had the chance to gain thousands of miles & territory & break down the Axis' Campaign.
+
+<h3> The Conclusion of WW2 After The Defeat of The Axis:
+<p> There were a few events after the WW2. These are what happened after.
+        <li> The leader of the Germany's Nazis, Adolf Hitler, commited sucuide by shooting himself.
+        <li> The Allies created the United Nations. An organization to stop anymore disaster wars like WWII.
+        <li> The Axis ended up joining the United Nations because of their defeat.
+</ul>
+
+
+</body>
+</html>
